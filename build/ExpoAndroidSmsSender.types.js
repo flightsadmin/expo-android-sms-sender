5527f1e3f97fb36f062ab38c0686e463913ce602
George Chitechi <mctapplications@gmail.com>
2026-09-09 17:24:42 +0300

    chore: keep original owner links in package.json and restore .gitignore

--- build/ExpoAndroidSmsSender.types.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=ExpoAndroidSmsSender.types.js.map
--- build/ExpoAndroidSmsSenderModule.d.ts
+++
@@ -1,8 +0,0 @@
-import { NativeModule } from 'expo';
-declare class ExpoAndroidSmsSenderModule extends NativeModule {
-    getSimCards(): Promise<string>;
-    sendSms(phoneNumber: string, text: string, simCardId?: number): Promise<void>;
-}
-declare const _default: ExpoAndroidSmsSenderModule;
-export default _default;
-//# sourceMappingURL=ExpoAndroidSmsSenderModule.d.ts.map
